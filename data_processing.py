0e4a2caca42ffc35d4e21e4b1e10da61fe2d940f
oli-dev-ai <oopyrchal@pm.me>
2026-04-14 21:49:55 +0200

    Processed and added three tables, drug_registry, drug_treatment and diagnosis

--- data_processing.py
+++ data_processing.py
@@ -48,5 +48,59 @@ def diag_data_processing():
     return save_diag
 
 
+def drug_treatment_data():
+    """Processind data about drug treatment"""
+    schema_overrides = {
+        "KOD_SWIADCZENIODAWCY": pl.String,
+        "KOD_EAN_LEKU": pl.String
+    }
+
+    df_drug = pl.read_csv("data_raw/rozpoznanieicd10lek.csv",
+                          encoding="utf8",
+                          separator=";",
+                          schema_overrides=schema_overrides
+                          )
+
+    result_drug = df_drug.select(
+        pl.col("KOD_SWIADCZENIODAWCY").cast(pl.String).alias("hospital_code"),
+        pl.col("ROZPOZNANIE_WG_ICD10").alias("diag_code"),
+        pl.col("KOD_EAN_LEKU").alias("drug_code"),
+        pl.when(pl.col("LICZBA_PACJENTOW") == "<5")
+        .then(pl.lit(None))
+        .otherwise(pl.col("LICZBA_PACJENTOW")).alias("patient_count"),
+        pl.when(pl.col("LICZBA_PACJENTOW") == "<5")
+        .then(True)
+        .otherwise(False).alias("is_suppressed")
+    )
+
+    save_drug = result_drug.write_csv("data_processed/drug_treatment.csv", separator=";")
+    return save_drug
+
+
+
+def drug_registry():
+    """Processing data from drug registry"""
+    df_registry = pl.read_csv("data_raw/rejestr_lekow.csv",
+                          encoding="utf8",
+                          separator=";",
+                          )
+
+    result_registry = (df_registry
+                       .select(
+        pl.col("Nazwa Produktu Leczniczego").alias("trade_name"),
+        pl.col("Nazwa powszechnie stosowana").alias("substance_name"),
+        pl.col("Opakowanie").str.split("\n").alias("ean_code")
+    )
+                       .explode("ean_code")
+                       .with_columns(pl.col("ean_code").str.split("¦"))
+                       .explode("ean_code")
+                       .with_columns(pl.col("ean_code").str.strip_chars())
+                       .filter(pl.col("ean_code").str.contains(r"^\d{13}", literal=False))
+                       )
+
+    save_registry = result_registry.write_csv("data_processed/drug_registry.csv", separator=";")
+    return save_registry
+
+
 if __name__ == "__main__":
-    diag_data = diag_data_processing()
+    drug_registry()
--- db_ops.py
+++ db_ops.py
@@ -2,11 +2,44 @@ import duckdb
 import polars
 
 
+def icd_dict_table():
+    """Main ICD-10 table with all information"""
+    with duckdb.connect("database/medbase.db") as con:
+        con.sql("""CREATE TABLE icd_codes AS
+        SELECT *
+        FROM read_csv("data_processed/icd10-dict.csv")
+        """)
 
-"""Main ICD-10 table with all information"""
-with duckdb.connect("database/medbase.db") as con:
-    con.sql("""CREATE TABLE icd_codes AS
-    SELECT *
-    FROM read_csv("data_processed/icd10-dict.csv")
-    """)
+
+
+def diagnosis_table():
+    """Main diagnosis table with all information"""
+    with duckdb.connect("database/medbase.db") as con:
+        con.sql("""CREATE TABLE diagnosis AS
+        SELECT *
+        FROM read_csv("data_processed/rozpoznanie.csv", types={'hospital_code': 'VARCHAR' })
+        """)
+
+
+def drug_treatment_table():
+    """Drug treatment and diagnosis table"""
+    with duckdb.connect("database/medbase.db") as con:
+        con.sql("""CREATE TABLE drug_treatment AS
+        SELECT *
+        FROM read_csv("data_processed/drug_treatment.csv", types={'hospital_code': 'VARCHAR', 'drug_code': 'VARCHAR'})
+        """)
+
+
+def drug_registry():
+    """Full registry of drugs in Poland"""
+    with duckdb.connect("database/medbase.db") as con:
+        con.sql("""CREATE TABLE drug_registry AS
+        SELECT *
+        FROM read_csv("data_processed/drug_registry.csv", types={'ean_code': 'VARCHAR'} )
+        """)
+
+
+if __name__ == "__main__":
+    drug_registry()
+
 
